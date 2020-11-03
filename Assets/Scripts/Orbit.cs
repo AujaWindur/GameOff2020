d89b764c67118242d4ae230f34ff43fcbfb91c08
Dan <danre4@gmail.com>
2020-11-03 22:35:15 +0100

    orbits

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public GameObject OrbitTarget;
+    public Vector3 RotationAxis = Vector3.up;
+    public float Speed = 10f;
+
+    void Update()
+    {
+        var lastRotation = transform.rotation;
+        transform.RotateAround(OrbitTarget.transform.position, RotationAxis.normalized, Speed * Time.deltaTime);
+        transform.rotation = lastRotation;
+    }
+}
